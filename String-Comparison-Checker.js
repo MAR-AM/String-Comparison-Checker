c066876e5c15ec3799dded35c8516a46b29ec95f
MARIEM-AMZIL <147107786+MAR-AM@users.noreply.github.com>
2024-04-28 13:34:33 +0100

    Create String-Comparison-Checker.js

---
+++ String-Comparison-Checker.js
@@ -0,0 +1,9 @@
+var a = prompt("Enter the first string : ")
+var b = prompt("Enter the second string : ")
+
+if (a==b){
+    document.write("The strings entered are identical.")
+}
+else{
+    document.write("The strings entered are different.")
+}
